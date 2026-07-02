e58206c5f8727109249198c393766581be12000b
Angelica Garcia <gbarron.angelica@gmail.com>
2026-07-02 16:27:48 -0600

    base de datos

---
+++ src/main/java/com/Retrozone/retrozone_bd/RetrozoneBdApplication.java
@@ -0,0 +1,13 @@
+package com.Retrozone.retrozone_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrozoneBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrozoneBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Retrozone/retrozone_bd/RetrozoneBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Retrozone.retrozone_bd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrozoneBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

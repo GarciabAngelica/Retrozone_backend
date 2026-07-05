fe7611c80d34f13f8e0b45d02c18b96ec223af0f
Aura Salazar <aurasader23@gmail.com>
2026-07-04 21:33:18 -0600

    se creo category

--- src/main/java/com/Retrozone/retrozone_bd/model/Category.java
+++ src/main/java/com/Retrozone/retrozone_bd/model/Category.java
@@ -1,4 +1,26 @@
 package com.Retrozone.retrozone_bd.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @NotBlank
+    private String categoryName;
+
+    @NotBlank
+    private String categoryDescription;
 }
